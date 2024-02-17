926ca0bffe9bf16703378d4d20c5125fb11c2af2
Leonardo Kunstmann <leokmaia@gmail.com>
2024-02-17 11:49:33 -0300

    alterando de 10 para 100

--- js-curso-2-aula1/js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ js-curso-2-aula1/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -5,7 +5,7 @@
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 //console.log (numeroSecreto);
